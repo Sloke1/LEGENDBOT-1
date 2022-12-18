3b5ba7912fcc84092a686e0840623c832e93c451
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-12-19 00:41:00 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,16 +3,16 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "Your Value"
+    APP_ID = 17881150
+    API_HASH = "83e83a14784a5355838dd0c24529d52a"
     # the name to display in your alive message
     ALIVE_NAME = "Your value"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgres://vbwadrur:IAzzBnNYWY7I9Pka9IoVQ8R_rR8NXJd2@mahmud.db.elephantsql.com/vbwadrur"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    LEGEND_STRING = "Your value"
+    LEGEND_STRING = "1AZWarzUBuxvy7WfsPwYssPg2r4fZI2qOFTAGfCO4p1yVN-qK77_xnrlBJg9vse3vLGhOBtPYlvJDSn5sW-3Uml2C2fEeM1XJXsL5k2jHc50aPHpgvDr5J2gdlPCH32HNoQnWOrYaX_pymBc0oTLmVcagwUmkWEdt1gyGyvCt75k_vJw3un_TFZBjWlJgKUKsRvTXkImcZY0rDgFXijl7wjO3Ict9EH5H6zHl69U3m3pPwmzk6yldfbUTcPQk_5Iv6lv6rOWy3xussxP0KimTmYjH5ELUDTvjJ_Pjw9pq8NsFwoG8hvXPsyOGqqjZQuR5UcOkIUead2NoX2hqpwWxoIThVs9uVPE="
     # create a new bot in @botfather and fill the following vales with bottoken
-    BOT_TOKEN = "Your value"
+    BOT_TOKEN = "5540365226:AAGaOTQYFSEhGqhRx0cFev6heCDI1JZqsCo"
     # command handler
     HANDLER = "."
     # command hanler for sudo
@@ -21,4 +21,4 @@ class Development(Config):
     EXTRA_REPO = "https://github.com/ITS-LEGENDBOT/PLUGINS"
     UPSTREAM_REPO = "pro"
     # Your City's TimeZone
-    TZ = "Your value"
+    TZ = "Asia/Kolkata"
